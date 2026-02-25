bf04c2e51a1d60b0ecb7c37a74aec7120ef623d7
emmanuelmj <emmanuelmj08@gmail.com>
2026-02-25 23:48:37 +0530

    feat(backend): configure alembic to load database URI dynamically from env

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -1,9 +1,14 @@
+import os
+from dotenv import load_dotenv
+
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+
+load_dotenv()
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -24,6 +29,8 @@ target_metadata = None
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
+
+config.set_main_option("sqlalchemy.url", os.environ.get("DATABASE_URL"))
 
 
 def run_migrations_offline() -> None:
